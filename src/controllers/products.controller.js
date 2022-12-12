14a6b908ad2160b5ffdd488497704400d65c314c
SrMazas <info.otero.gabriel@gmail.com>
2022-12-12 13:46:31 +0100

    debug

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -72,7 +72,7 @@ export const patchProduct = async (req, res) => {
 
         res.sendStatus(204);
     } catch (error) {
-        return res.status(500).json({ message: req.body});
+        return res.status(500).json({ message: `UPDATE products SET name=IFNULL(${name},name),IFNULL(${image},image),stock=IFNULL(${stock},stock),target_stock=IFNULL(${target_stock},target_stock),ref_alcampo=IFNULL(${ref_alcampo},ref_alcampo),ref_carrefour=IFNULL(${ref_carrefour},ref_carrefour) WHERE id=${req.params.id}`});
     }
 };
 
